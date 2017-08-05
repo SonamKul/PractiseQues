32912068f8ad9f0ff259930befbcbc6e06dac4d1
SonamKul <kulsonam28@gmail.com>
2017-08-05 18:04:55 +0530

    Even Numbers List

---
+++ EvenList.py
@@ -0,0 +1,10 @@
+array_list = [5,6,27,28,16,3,67,24,88,11,39,56,101,990,152,843,975,456,1192]
+print "Even Numbers from the system generted list"
+
+new_list = []
+for i in array_list :
+    if i%2 == 0 :
+        new_list.append(i)
+print new_list
+
+
