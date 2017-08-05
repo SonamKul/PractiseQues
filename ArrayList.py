35fe136560673f2e3284f06981c67395c6261a6c
SonamKul <kulsonam28@gmail.com>
2017-08-05 16:39:55 +0530

    ListIndex

---
+++ ArrayList.py
@@ -0,0 +1,4 @@
+array_list = [' Ferrari Pininfarina','BMW','Audi','Lamborghini','McLaren P1 LM ','Ferrari']
+print "Number of elements in the array are 5"
+array = int(raw_input("Enter the index whose element you want to check : "))
+print array_list[array]
