fe278e7371206039732deb9a7307ced8fde0651a
SonamKul <kulsonam28@gmail.com>
2017-08-12 23:59:44 +0530

    Concate

--- Count.py
+++ Count.py
@@ -1,3 +1,6 @@
-str = raw_input("Enter a string : \n")
-length = str.__len__()
-print length
+str = raw_input("Enter your name : \n")
+age = int(raw_input("Enter your age please : "))
+rating = float(raw_input("Enter the spy rating : "))
+address = raw_input("Enter your address : \n")
+
+print "The Spy details are :\n Name of spy is %s" % str +" ,whose age is %d" % age + " and \n is having a Spy rating of %f " %rating + " and is a permanent resident of %s " %address
