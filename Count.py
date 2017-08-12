55452a80dca00718d705a095e1c8a4d854f3108f
SonamKul <kulsonam28@gmail.com>
2017-08-13 02:01:10 +0530

    Practise Questions

--- Count.py
+++ Count.py
@@ -3,4 +3,4 @@ age = int(raw_input("Enter your age please : "))
 rating = float(raw_input("Enter the spy rating : "))
 address = raw_input("Enter your address : \n")
 
-print "The Spy details are :\n Name of spy is %s" % str +" ,whose age is %d" % age + " and \n is having a Spy rating of %f " %rating + " and is a permanent resident of %s " %address
+print "The Spy details are :\n Name of spy is %s" % str +" ,whose age is %d" % age + " and is having a Spy rating of %f " %rating + " and is a permanent resident of %s " %address
