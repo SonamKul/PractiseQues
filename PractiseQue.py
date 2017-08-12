349441bb340f86ca52ff6101a6a2627c94657431
SonamKul <kulsonam28@gmail.com>
2017-08-13 01:47:02 +0530

    Practise Ques

---
+++ PractiseQue.py
@@ -0,0 +1,6 @@
+import time
+print "Enter any string and get it's character at 1st index."
+time.sleep(1)
+str = raw_input("Enter any string : \n")
+print "The string is : " + str
+print "The alphabet at 0 index is : " + str[0]
