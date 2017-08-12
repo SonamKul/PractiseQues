002347570c989de079941807f0870d270d4dd601
SonamKul <kulsonam28@gmail.com>
2017-08-12 23:48:25 +0530

    Upper and Lower case

--- Uppercase.py
+++ Uppercase.py
@@ -1,8 +1,15 @@
 import time
-print "Enter a string and change it to capital letters."
+print "Enter a string and change it to Upper case or Lower case as per your choice \n Enter 1 - Upper case String. \n 2 - Lower case"
 
 time.sleep(1)
-str = raw_input("Enter a String : ")
+str = raw_input("Enter a String : \n")
 time.sleep(1)
-new_str = str.upper()
-print new_str
+
+choice = int(raw_input("Enter your choice : \n"))
+if choice == 1 :
+    new_str = str.upper()
+    print new_str
+elif choice == 2 :
+    new_str = str.lower()
+    print new_str
+
