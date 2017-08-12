0c27efc730a1abdd8dd8cb643da23ad57cabcb97
SonamKul <kulsonam28@gmail.com>
2017-08-13 01:59:02 +0530

    Practise Questions

--- PractiseQue.py
+++ PractiseQue.py
@@ -1,7 +1,15 @@
 import time
 print "Enter any string and get it's character at 1st index."
-time.sleep(1)
+time.sleep(2)
 str = raw_input("Enter any string : \n")
+time.sleep(2)
 print "The string is : " + str
+time.sleep(2)
 print "The alphabet at 0 index is : " + str[0]
-print str[2:3]
+time.sleep(2)
+print "Printing the string [2:3] value : \n" + str[2:3]
+time.sleep(2)
+print "Printing the string [2:] value : \n" + str[2:]
+time.sleep(2)
+print "Printing the string (string * 2) value : \n" + (str*2)
+time.sleep(2)
