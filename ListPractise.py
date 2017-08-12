a345d542f2ba1c5c0f10d7ba3bd8ca49b707ae58
SonamKul <kulsonam28@gmail.com>
2017-08-13 02:14:46 +0530

    List Practise Ques

---
+++ ListPractise.py
@@ -0,0 +1,11 @@
+import time
+list = ['Sonam',786,80.2,'Vikram',70.2]
+print "List is :\n " , list
+time.sleep(2)
+print "Index 0 in the list is having the value : " + list[0]
+time.sleep(2)
+print "The list at [1:3] is : " , list[1:3]
+time.sleep(2)
+print "The list at [2:] is : " , list[2:]
+time.sleep(2)
+print "The list at [1:3] is : " , list[1:3]
